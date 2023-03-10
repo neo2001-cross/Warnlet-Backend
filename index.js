4a384b1ea36d5ef2e37cf9e1e994ed6f6bdd3b77
neo2001-cross <ngochuy11010@gmail.com>
2023-03-10 23:18:35 +0700

    update structure database

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ app.get('/api/warnlet', (req, res) => {
     res.json({
         name: "Nguyen Le Ngoc Huy",
         money: {
-            cash: 75000,
+            cash: 0,
             EWallet: 0,
-            banking: 2100000 
+            banking: 2127000 
+        },
+        history: [
+            {"date": "10-02-2023", "content": "mua monsgeeks m1"},
+            {"date": "10-03-2023", "content": "mua keycap Chicago"}
+        ],
+        transaction:{
+            "Uyên Nguyệt Chúc Phúc genshin": 109000,
+            "Phí Netflix": 89000
         }
     })
 })
